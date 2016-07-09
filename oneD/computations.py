8dae5ce27aad8e800d6c02b5ee0d3a03f5fc3dd4
yaoyuan5 <yy.yao@mail.utoronto.ca>
2016-07-09 19:38:53 -0400

    Update computations.py

--- oneD/computations.py
+++ oneD/computations.py
@@ -18,14 +18,27 @@ def pot1(x,t):
         return 0.1 * x ** 2 + x
 
 def pot2(x):
-    if abs(x) < L * 0.1:
-        return 0
+    if abs(x) < 2.:
+        return 0.1
     else:
-        return 10
+        return 0
 
 def pot3(x):
-    return abs(x)
-        
+    return 0.2*abs(x)
+
+def pot4(x):
+    if abs(x) < 2.:
+        return -0.2
+    else:
+        return 0
+
+def pot5(x): #we thank the mentor for this idea
+    if abs(x)>4*pi:
+        return 0
+    if abs(x)<= 4*pi:
+        return 3*sin(x/4.)
+def pot6(x):
+    return x**2-0.0001j
 
 
 '''
@@ -35,9 +48,10 @@ representing the input, it will take 2D array V_tx the represents the potential.
 it should should also take input integer k that tells you at what timestep you are doing this step.
 '''
 
-def nonlinear(f_ini,V,k):
+def nonlinear_t(f_ini,V,k):
+    return f_ini*exp(-1.j*V[k]*dt/hbar)
+def nonlinear_nt(f_ini,V,k):
     return f_ini*exp(-1.j*V*dt/hbar)
-
 
 '''
 linear fourier step
@@ -46,4 +60,8 @@ def linear(f_ini):
     F1=fft(f_ini)*exp(-1.j*hbar*4.*pi**2*freq_ns**2*dt/(2*m))
     return ifft(F1)
 
+
+
+
  
+
