f0357d7655fc27e324902ca4992f4e438e89a082
Lennart Doeppenschmitt <lennart@math.toronto.edu>
2016-07-09 11:23:09 -0400

    initial commit

---
+++ oneD/main.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+from computations import *
+from constants import *
+from fileWriter import *
+
+from numpy import *
+from pylab import *
+from matplotlib import animation
